9cb3a552c2cdaeca83c2ccf6c1dc8529ae970bba
Eric McCorkle <eric@metricspace.net>
2017-10-06 20:48:46 -0400

    Add ChaCha20 JCA API test

---
+++ src/test/unit/java/net/metricspace/crypto/ciphers/stream/salsa/ChaCha20JCACipherTest.java
@@ -0,0 +1,75 @@
+/* Copyright (c) 2017, Eric McCorkle.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.metricspace.crypto.ciphers.stream.salsa;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.ShortBufferException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import net.metricspace.crypto.ciphers.JCACipherTest;
+import net.metricspace.crypto.providers.KryptonProvider;
+
+public class ChaCha20JCACipherTest extends JCACipherTest {
+    @BeforeClass
+    public static void init() {
+        KryptonProvider.register();
+    }
+
+    @AfterClass
+    public static void fini() {
+        KryptonProvider.unregister();
+    }
+
+    public ChaCha20JCACipherTest() {
+        super(KryptonProvider.NAME, ChaCha20CipherSpi.NAME, 256, 64);
+    }
+
+    @Test
+    @Override
+    protected void jcaCipherSmokeTest()
+        throws ShortBufferException, InvalidKeyException,
+               NoSuchAlgorithmException, NoSuchProviderException,
+               InvalidAlgorithmParameterException, IllegalBlockSizeException,
+               NoSuchPaddingException, BadPaddingException {
+        super.jcaCipherSmokeTest();
+    }
+}
